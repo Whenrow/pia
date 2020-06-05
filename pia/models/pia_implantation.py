676c09756626ab93a06a7db98e05c69e047da867
William Henrotin <whe@odoo.com>
2020-06-05 19:57:49 +0200

    [FIX] pia: recompute allowed_implantation_ids at implantation creation
    As the allowed_implantation_ids computed field has not any depends
    fields, this fields was not recomputed when a new implantation was
    created. This lead to an access error if this implantation was used in
    another record.
    
    This commit force the re computation for the current user at each new
    implantation creation in order  to avoid those unwanted access errors.

--- pia/models/pia_implantation.py
+++ pia/models/pia_implantation.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class Implantation(models.Model):
@@ -19,3 +19,10 @@ class Implantation(models.Model):
     telephone = fields.Char()
     gsm = fields.Char()
     email = fields.Char()
+
+    @api.model
+    def create(self, vals):
+        res = super().create(vals)
+        # Hacky tricks to recompute the allowed implantation ASAP in case it is use right after
+        self.env.user._compute_allowed_implantation_ids()
+        return res
