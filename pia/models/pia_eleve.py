7b78900e718d13863a90499ff452d848b125880b
William Henrotin <whe@odoo.com>
2020-09-09 22:11:44 +0200

    [IMP] pia: menu revamp

--- pia/models/pia_eleve.py
+++ pia/models/pia_eleve.py
@@ -18,6 +18,8 @@ class Eleve(models.Model):
     conseil_count = fields.Integer(compute='_compute_conseil_count')
     reunion_count = fields.Integer(compute='_compute_conseil_count')
     synthese_count = fields.Integer(compute='_compute_conseil_count')
+    bilan_count = fields.Integer(compute='_compute_conseil_count')
+    entretien_count = fields.Integer(compute='_compute_conseil_count')
 
     # Tuteurs
     nom_tuteur_1 = fields.Char('Nom')
@@ -62,18 +64,26 @@ class Eleve(models.Model):
 
     def _compute_conseil_count(self):
         for eleve in self:
-            synthese_count = self.env['pia.synthese.logo'].sudo().search_count([
+            synthese_count = self.env['pia.synthese.logo'].sudo().sudo().search_count([
                 ('eleve_id', '=', eleve.id)
             ])
-            reunion_count = self.env['pia.reunion.parents'].search_count([
+            reunion_count = self.env['pia.reunion.parents'].sudo().search_count([
                 ('eleve_id', '=', eleve.id)
             ])
-            conseil_count = self.env['pia.conseil'].search_count([
+            conseil_count = self.env['pia.conseil'].sudo().search_count([
+                ('eleve_id', '=', eleve.id)
+            ])
+            bilan_count = self.env['pia.bilan.logo'].sudo().search_count([
+                ('eleve_id', '=', eleve.id)
+            ])
+            entretien_count = self.env['pia.entretien'].sudo().search_count([
                 ('eleve_id', '=', eleve.id)
             ])
             eleve.conseil_count = conseil_count
             eleve.reunion_count = reunion_count
             eleve.synthese_count = synthese_count
+            eleve.bilan_count = bilan_count
+            eleve.entretien_count = entretien_count
 
     def action_view_synthese(self):
         self.ensure_one()
@@ -96,6 +106,19 @@ class Eleve(models.Model):
         action['context'] = dict(self._context, default_eleve_id=self.id)
         return action
 
+    def action_view_entretien(self):
+        self.ensure_one()
+        action = self.env.ref('pia.action_view_entretien').read()[0]
+        action['domain'] = [('eleve_id', '=', self.id)]
+        action['context'] = dict(self._context, default_eleve_id=self.id)
+        return action
+
+    def action_view_bilan(self):
+        self.ensure_one()
+        action = self.env.ref('pia.action_view_bilan').read()[0]
+        action['domain'] = [('eleve_id', '=', self.id)]
+        action['context'] = dict(self._context, default_eleve_id=self.id)
+        return action
 
 class Trouble(models.Model):
     _name = 'pia.trouble'
