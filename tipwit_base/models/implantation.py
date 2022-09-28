c1055873952c03ebd63075a779082a900ae51b73
William Henrotin <whe@odoo.com>
2022-09-28 21:24:04 +0200

    [FIX] base: don't blick user when creating implantation at eleve creation

--- tipwit_base/models/implantation.py
+++ tipwit_base/models/implantation.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import api, models, fields
 
 
 class Implantation(models.Model):
@@ -21,3 +21,9 @@ class Implantation(models.Model):
     telephone = fields.Char()
     gsm = fields.Char()
     email = fields.Char()
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        res = super().create(vals_list)
+        self.env.user._compute_allowed_implantation_ids()
+        return res
